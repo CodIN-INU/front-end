0c786e24711c0446528a80f5969dbe0dc0d5b5db
ParkKyungWan <pkw8117197@gmail.com>
2025-07-16 02:02:20 +0900

    test: 로그인 오류 해결중

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -145,8 +145,11 @@ export default function LoginPage() {
   ): Promise<void> => {
     e.preventDefault();
     try {
-
-      const href = `https://codin.inu.ac.kr/api/auth/google?redirect_url=https://front-end-dun-mu.vercel.app`;
+      const href = `https://codin.inu.ac.kr/api/auth/google`;
+      if(process.env.NEXT_PUBLIC_ENV === 'dev'){
+        const redirectUri = "https://codin.inu.ac.kr/login/dev";
+        const href = `https://codin.inu.ac.kr/api/auth/google?redirect_url=${encodeURIComponent(redirectUri)}`;
+      }
 
       if (!isLoginPressed) {
         console.log('로그인 요청 중...')
