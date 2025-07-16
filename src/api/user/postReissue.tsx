162201c66464c3c7cf2bad40d0e6812d39934ecb
ParkKyungWan <pkw8117197@gmail.com>
2025-07-16 16:55:53 +0900

    feat: Reissue with localstorage

--- src/api/user/postReissue.tsx
+++ src/api/user/postReissue.tsx
@@ -10,6 +10,18 @@ export const PostReissue = async (): Promise<any> => {
       `${apiUrl}/auth/reissue`
     );
 
+    if( process.env.NEXT_PUBLIC_ENV === 'dev'){
+
+      // ✅ 서버에서 받은 JWT 토큰을 헤더에서 추출
+      const token = response.headers["authorization"].split(" ")[1];
+      alert("토큰 재발급 성공: " + token);
+
+      if (token) {
+        // ✅ JWT를 localStorage에 저장 (WebView-safe)
+        localStorage.setItem("accessToken", token);
+      }
+    }
+
     console.log(response.data);
     console.log(response.headers);
 
