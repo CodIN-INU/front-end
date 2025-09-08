3f845621d3ec5137f7e38d82ccbb27006c75e614
moridodos <coderdhk06@gmail.com>
2025-09-08 20:23:10 +0900

    chore: useSearchParams Suspense

--- src/app/(auth-required)/dept-boards/(nav)/layout.tsx
+++ src/app/(auth-required)/dept-boards/(nav)/layout.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from 'react';
 import DeptBoardsHeader from './header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <Suspense>
       <DeptBoardsHeader>{children}</DeptBoardsHeader>
-    </>
+    </Suspense>
   );
 }
