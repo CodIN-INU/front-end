3fb8c5a2fa09adcf3ddf3ac583c595ed61926d04
yereong <lily030624@gmail.com>
2025-11-24 03:49:54 +0900

    fix: 데이터 인터페이스 기존에 있는 것들로 변경

--- src/app/admin/ticketing/create/types/inputBlockProps.ts
+++ src/app/admin/ticketing/create/types/inputBlockProps.ts
@@ -1,10 +1,10 @@
-import { TicketForm } from "./ticketForm";
+import { TicketEvent } from "@/interfaces/SnackEvent";
 import { ChangeEvent } from "react";
 
 export type InputBlockProps = {
   label: string;
-  name: keyof TicketForm;
-  value: string;
+  name: keyof TicketEvent;
+  value: string | number;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   type?: string;
