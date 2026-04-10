aae42a6e24840b035106cdfe9430eb37f3c194c6
yereong <lily030624@gmail.com>
2026-04-11 01:01:03 +0900

    fix: 깨진 한글 복구

--- src/features/ticketing/components/modals/ChangeStatusModal.tsx
+++ src/features/ticketing/components/modals/ChangeStatusModal.tsx
@@ -44,7 +44,7 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
     if (!userInfo) return;
     
     if (sigCanvasRef.current?.isEmpty()) {
-        alert('??????.');
+        alert('서명을 입력해주세요.');
         return;
       }
   
@@ -65,16 +65,16 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
         body:  formData,
       });
       
-      console.log('?? ?? ??? ?? ??:', response);
-      alert('?? ???????.');
+      console.log('✅ 유저 정보 업데이트 성공:', response);
+      alert('수령 완료로 변경하였습니다');
       onComplete();
     } catch (error) {
-      console.error('?? ?? ??? ?? ??:', error);
+      console.error('❌ 유저 정보 업데이트 실패:', error);
     }
   };
 
   const handleCancle = async()=>{
-    const confirmed = confirm('??? ?? ?? ??? ?????????');
+    const confirmed = confirm('해당 유저의 티켓을 취소하시겠습니까?');
     if (!confirmed) return;
 
     try{
@@ -82,11 +82,11 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
         ,{
             method: 'DELETE'
           })
-      alert('???????.');
+      alert('취소하였습니다');
       window.location.reload();
     }catch(error){
       console.error();
-      alert('?? ? ??? ??????. ?? ??????.');
+      alert('취소 중 오류가 발생했습니다. 다시 시도해주세요');
       window.location.reload();
     }
     
@@ -97,7 +97,7 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
     <div className="fixed inset-0 backdrop-blur-sm bg-black/50 flex items-center justify-center z-[70] ">
         <div className="bg-white rounded-2xl w-[80%] px-6 py-[14px] shadow-xl text-center relative max-w-[500px]">
 
-          {/* ?? ?? */}
+          {/* 닫기 버튼 */}
         <button className="absolute top-3 right-3 text-gray-400" onClick={onClose}>
           ?
         </button>
@@ -105,14 +105,14 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
         {step === 1 && (
           <>
       
-        {/* ??? ????? */}
+        {/* 페이지 인디케이터 */}
             <div className="flex justify-center mb-[11px]">
               <span className="w-[10px] h-[10px] bg-[#0D99FF] rounded-full" />
               <span className='w-[18px] h-[1px] border border-[#D9D9D9] mt-[3px]'></span>
               <span className="w-[10px] h-[10px] bg-[#D9D9D9] rounded-full" />
             </div>
         
-        {/* ??? ?? */}
+        {/* 사용자 정보 */}
 
             <div className=' flex flex-col'>
                 <div className='text-[12px] text-[#79797B]'>{userInfo.department}</div>
@@ -122,28 +122,28 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
 
            
         
-        {/* ?? */}
+        {/* 버튼 */}
 
             <div className='flex flex-row gap-[15px] items-center justify-center mt-[13px]'>
                 <button className='w-[119px] h-10 bg-[#EBF0F7] rounded-[5px] text-[#808080] text-[14px]'
-                        onClick={handleCancle}>?? ??</button>
+                        onClick={handleCancle}>수령 취소</button>
 
                 <button className='w-[119px] h-10 bg-[#0D99FF] rounded-[5px] text-white text-[14px]'
-                        onClick={()=>setStep(2)}>?? ??</button>
+                        onClick={()=>setStep(2)}>수령 완료</button>
             </div>
       </>)}
 
-       {/* STEP 2 - ?? ?? */}
+       {/* STEP 2 서명*/}
         {step === 2 && (
           <div className='flex flex-col'>
-            {/* ??? ????? */}
+            {/* 페이지 인디케이터 */}
             <div className="flex justify-center mb-3">
               <span className="w-[10px] h-[10px] bg-[#D9D9D9] rounded-full" />
               <span className='w-[18px] h-[1px] border border-[#D9D9D9] mt-[3px]'></span>
               <span className="w-[10px] h-[10px] bg-[#0D99FF] rounded-full" />
             </div>
 
-             {/* ??? ?? */}
+             {/* 사용자 정보 */}
 
             <div className=' flex flex-col mb-[14px]'>
                 <div className='text-[12px] text-[#79797B]'>{userInfo.department}</div>
@@ -151,7 +151,7 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
                 <div className='text-[12px] text-[#0D99FF] font-bold'>{userInfo.studentId}</div>
             </div>
 
-            {/* ?? ??? */}
+            {/* 서명 캔버스 */}
                     <div className='flex flex-col justify-center '>
                         <div className="bg-white w-full h-[160px] rounded-xl shadow-[0px_5px_13.3px_4px_rgba(212,212,212,0.59)] mb-2 flex items-center justify-center">
                         <div className='absolute top-[113px] left-8 text-black text-[14px] font-bold flex'>??<div className="text-blue-500 text-[22px] mt-[-15px] ml-1">?</div></div>
@@ -163,20 +163,20 @@ const ChangeStatusModal: FC<ChangeStatusModalProps> = ({ onClose, onComplete, us
                             penColor="black"
                         />
                         {sigCanvasRef.current?.isEmpty() && (
-                            <span className="absolute text-sm text-gray-400 ">??????</span>
+                            <span className="absolute text-sm text-gray-400 ">서명을 해주세요</span>
                             )}
                         </div>
                         <button className='absolute top-[235px] right-8 text-[10px] text-[#808080] bg-[#EBF0F7] rounded-[5px] px-[10px] py-1'
-                                onClick={handleClear}>?? ??</button>
+                                onClick={handleClear}>다시 쓰기</button>
             
-                        <p className="text-center text-[12px] text-black/50 mt-2 mb-4">?? ?? ?? ? ?? ???? ?????.</p>
+                        <p className="text-center text-[12px] text-black/50 mt-2 mb-4">수령 확인 및 본인 확인 용도로 사용됩니다.</p>
                     </div>
             <button
               className='w-full bg-[#0D99FF] text-white text-[14px] py-[10px] rounded-[5px]'
               disabled={!sigCanvasRef}
               onClick={postUserSign}
             >
-              ??
+              확인
             </button>
           </div>
         )}
