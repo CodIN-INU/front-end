d14bef61db7ba70d9c119c8b6e6a406d9d25f011
yereong <lily030624@gmail.com>
2026-04-11 14:52:18 +0900

    feat: 학과 한글로 바꿔주는 유틸 추가

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './compressBase64Image';
 export * from './getApiErrorMessage';
+export * from './getDepartmentLabel';
 export * from './convertToKoreanDate';
 export * from './dataUrlToFile';
 export * from './date';
